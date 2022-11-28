b2e1c09c1cf5990b11a5a4f4a16bfdb4cef9e9ed
Cormac Smith <cormacsmith@live.co.uk>
2022-11-28 23:04:53 +0000

    note styling

--- lab3/problem3/my-app/src/App.js
+++ lab3/problem3/my-app/src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import { Text } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
   return (
-    <p>hello</p>
+    <div className="App">
+      <VStack spacing='10px'>
+        <div
+        style={{
+          width: "300px",
+          margin: "20px auto",
+          background: "green",
+          border: "1px solid #333",
+        }}
+      >
+        <p>Note ID: 1</p>
+        <p>soaivhoashvioashvioahoihsaovhasiov</p>
+        <ButtonGroup variant='outline' spacing='6'>
+          <Button colorScheme='blue'>Edit</Button>
+          <Button>Delete</Button>
+        </ButtonGroup>
+        </div>
+      </VStack>
+    </div>
   );
 }
 
